a48e52ba6e74fb36b4b1bbda22a8aed3cb880218
NoNotNow <riviere.manuel@gmail.com>
2025-08-11 10:08:52 +0200

    Load saved code from cookies at startup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // Main application entry point
 import { updateStageView, updateView } from './view-renderer.js';
 import { setupEventListeners } from './event-handlers.js';
+import { loadCode } from './save-load.js';
 
 function main() {
   setupEventListeners();
   updateStageView();
   updateView();
+  loadCode();
 }
 
 // Wait for DOM to be fully loaded before initializing
